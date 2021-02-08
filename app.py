f177e1d3b202acc0c020a4e225393abdaf58b200
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2021-02-08 20:20:29 +0100

    Display temperature and humidity

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
+from datetime import datetime
+import board
+import adafruit_dht
+
+# TODO: Make ports configurable
+dht_device = adafruit_dht.DHT22(board.D14)
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    temperature = dht_device.temperature
+    humidity = dht_device.humidity 
+    current_time = datetime.now().strftime("%H:%M:%S")
+
+    return f'Temperature {temperature}°C and humidity {humidity}% as of {current_time}.'
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=80, host='0.0.0.0')
+    app.run(port=80, host='0.0.0.0')
